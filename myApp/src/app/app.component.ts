36fc271ceb870ef2672b63e1ae86332dbee7145b
breonwilliams <bjwilli83@gmail.com>
2017-07-18 18:58:33 -0400

    add manage client page

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ManageClientsPage } from '../pages/manage-clients/manage-clients';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Manage Clients', component: ManageClientsPage }
     ];
 
   }
