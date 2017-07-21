f36b3b398d655ebef9fa669b2b0c7b0d7416373f
breonwilliams <bjwilli83@gmail.com>
2017-07-21 08:01:15 -0400

    modify home template

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ManageClientsPage } from '../manage-clients/manage-clients';
+import { RecentScreensPage } from '../recent-screens/recent-screens';
+
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
+
+  params: Object;
+  pushPage: any;
+  pushPage1: any;
+
+
   constructor(public navCtrl: NavController) {
+    this.pushPage = ManageClientsPage;
+    this.pushPage1 = RecentScreensPage;
+    this.params = { id: 42 };
+
 
   }
 
