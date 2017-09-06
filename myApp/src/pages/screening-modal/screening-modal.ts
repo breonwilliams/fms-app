cbf9573d053e11e90fb2cded8818c45323b3bfc9
breonwilliams <bjwilli83@gmail.com>
2017-09-06 14:50:40 -0400

    add slide images

--- myApp/src/pages/screening-modal/screening-modal.ts
+++ myApp/src/pages/screening-modal/screening-modal.ts
@@ -23,8 +23,6 @@ export class ScreeningModalPage {
     var characters = [
       {
         name: 'Deep Squat',
-        quote: 'Sneaky little hobbitses!',
-        image: '../../assets/img/user-placeholder.svg',
         items: [
           { image:'../../assets/img/deep-squat3-slide.jpg', title: '3', note: 'Torso is parallel with tibia or toward vertical. Femur is below horizontal. Knees do not track inside of feet. Dowel aligned over feet.' },
           { image:'../../assets/img/deep-squat2-slide.jpg', title: '2', note: 'Torso is parallel with tibia or toward vertical. Femur is below horizontal. Knees do not track inside of feet. Dowel aligned over feet. Heels are elevated.' },
@@ -36,39 +34,33 @@ export class ScreeningModalPage {
         quote: 'Go back, Sam! I\'m going to Mordor alone!',
         image: '../../assets/img/user-placeholder.svg',
         items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Weapon', note: 'Sting' }
+          { image:'../../assets/img/hurdle-step3-slide.jpg', title: '3', note: 'Hips, knees and ankles remain aligned in the sagittal plane. Minimal to no movement in lumbar spine. Dowel and hurdle remain parallel.' },
+          { image:'../../assets/img/hurdle-step2-slide.jpg', title: '2', note: 'Alignment is lost between hips, knees and ankles. Movement in Lumbar Spine. Dowel and hurdle do not remain parallel.' },
+          { image:'../../assets/img/hurdle-step1-slide.jpg', title: '1', note: 'Inability to clear the cord during the hurdle step. Loss of Balance.' }
         ]
       },
       {
         name: 'Inline Lunge',
-        quote: 'What we need is a few good taters.',
-        image: '../../assets/img/user-placeholder.svg',
         items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Nickname', note: 'Sam' }
+          { image:'../../assets/img/inline-lunge3-slide.jpg', title: '3', note: 'Dowel contact maintained. Dowel remains vertical. Minimal to no torso movement. Dowel and feet remain in sagittal plane. Knee touches the center of the board. Front foot remains in start position.' },
+          { image:'../../assets/img/inline-lunge2-slide.jpg', title: '2', note: 'Dowel contact not maintained. Dowel does not remain vertical. Movement in torso. Dowel and feet do not remain in sagittal plane. Knee does not touch center of the board. Flat front foot does not remain in start position.' },
+          { image:'../../assets/img/inline-lunge1-slide.jpg', title: '1', note: 'Loss of balance by stepping off the board. Inability to complete movement pattern. Inability to get into set up position.' }
         ]
       },
       {
         name: 'Shoulder Mobility',
-        quote: 'What we need is a few good taters.',
-        image: '../../assets/img/user-placeholder.svg',
         items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Nickname', note: 'Sam' }
+          { image:'../../assets/img/shoulder-mobility3-slide.jpg', title: '3', note: 'Fists are within one hand length.' },
+          { image:'../../assets/img/shoulder-mobility2-slide.jpg', title: '2', note: 'Fists are within one and a half hand lengths.' },
+          { image:'../../assets/img/shoulder-mobility1-slide.jpg', title: '1', note: 'Fists are not within one and a half hand lengths.' }
         ]
       },
       {
         name: 'Active Straight-Leg Raise',
-        quote: 'What we need is a few good taters.',
-        image: '../../assets/img/user-placeholder.svg',
         items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Nickname', note: 'Sam' }
+          { image:'../../assets/img/active-straight-leg-raise3-slide.jpg', title: '3', note: 'Vertical line of the malleolus resides between mid-thigh and ASIS. The non-moving limb remains in neutral position.' },
+          { image:'../../assets/img/active-straight-leg-raise2-slide.jpg', title: '2', note: 'Vertical line of the malleolus resides between mid-thigh and mid-patella. The non-moving limb remains in neutral position.' },
+          { image:'../../assets/img/active-straight-leg-raise1-slide.jpg', title: '1', note: 'Vertical line of the malleolus resides below the mid-patella. The non-moving limb remains in neutral position.' }
         ]
       },
       {
@@ -76,19 +68,17 @@ export class ScreeningModalPage {
         quote: 'What we need is a few good taters.',
         image: '../../assets/img/user-placeholder.svg',
         items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Nickname', note: 'Sam' }
+          { image:'../../assets/img/trunk-stability-pushup3-slide.jpg', title: '3', note: 'Men perform a repetition with thumbs aligned with the top of the forehead. Women perform a repetition with thumbs aligned with the chin. The body lifts as a unit with no lag in the spine.' },
+          { image:'../../assets/img/trunk-stability-pushup2-slide.jpg', title: '2', note: 'Men perform a repetition with thumbs aligned with the chin. Women perform a repetition with thumbs aligned with the clavicle. The body lifts as a unit with no lag in the spine.' },
+          { image:'../../assets/img/trunk-stability-pushup1-slide.jpg', title: '1', note: 'Men are unable to perform a repetition with thumbs aligned with the chin. Women are unable to perform a repetition with thumbs aligned with the clavicle.' }
         ]
       },
       {
         name: 'Rotary Stability',
-        quote: 'What we need is a few good taters.',
-        image: '../../assets/img/user-placeholder.svg',
         items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Nickname', note: 'Sam' }
+          { image:'../../assets/img/rotary-stability3-slide.jpg', title: '3', note: 'Performs a correct unilateral repetition. Unilateral limbs remain over the board. Without touching down, touch the same-side elbow to the same-side knee over the board.' },
+          { image:'../../assets/img/rotary-stability2-slide.jpg', title: '2', note: 'Performs a correct diagonal repetition. The diagonal knee and elbow meet over the board. Without touching down, touch the opposite elbow and knee over the board.' },
+          { image:'../../assets/img/rotary-stability1-slide.jpg', title: '1', note: 'Inability to perform a diagonal repetition.' }
         ]
       }
     ];
